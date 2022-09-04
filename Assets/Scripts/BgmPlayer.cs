956a4a0d7d8b8d330c014b3ae805ec973beaffbd
world29 <623p6236p@gmail.com>
2022-09-04 21:06:44 +0900

    BGM 再生

---
+++ Assets/Scripts/BgmPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity1Week
+{
+    // タイムラインから BGM を再生するのに使う
+    public class BgmPlayer : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioClip bgm;
+
+        [SerializeField]
+        private float fadeTime = 0f;
+
+        void Start()
+        {
+            PlayBgm(bgm);
+        }
+
+        public void PlayBgm(AudioClip clip)
+        {
+            SoundManager.PlayBgm(clip, fadeTime);
+        }
+    }
+}
