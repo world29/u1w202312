014d49aa47f8cd994e25e7d7d839d135f8f59a7a
world29 <623p6236p@gmail.com>
2022-09-09 01:26:16 +0900

    プラットフォーム毎にアニメーション再生開始位置をオフセット

---
+++ Assets/Scripts/AnimatorRandomOffset.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Unity1Week
+{
+    public class AnimatorRandomOffset : MonoBehaviour
+    {
+        private Animator _animator;
+
+        void Awake()
+        {
+            TryGetComponent(out _animator);
+        }
+
+        void Start()
+        {
+            _animator.Play(_animator.GetCurrentAnimatorStateInfo(0).shortNameHash, 0, Random.Range(0f, 1f));
+        }
+    }
+}
