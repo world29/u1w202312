99f891712d29a674f1baf7d2deb7ce0afe2ce24c
world29 <623p6236p@gmail.com>
2022-09-11 17:30:04 +0900

    コミット漏れ

---
+++ Assets/Scripts/ScriptableObjects/ItemEnemySetting.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Unity1Week
+{
+    [CreateAssetMenu]
+    public class ItemEnemySetting : ScriptableObject
+    {
+        [System.Serializable]
+        public class ItemInfo
+        {
+            public Item prefab;
+
+            public int weight;
+        }
+
+        [System.Serializable]
+        public class EnemyInfo
+        {
+            public GameObject prefab;
+
+            public int weight;
+        }
+
+        [System.Serializable]
+        public struct Phase
+        {
+            [Tooltip("このスコア以上のとき、以下の設定値が採用される")]
+            public float score;
+
+            public float itemDropPercent;
+
+            public List<ItemInfo> itemLootTable;
+
+            public float enmeySpawnPercent;
+
+            public List<EnemyInfo> enemyLootTable;
+        }
+
+        [SerializeField]
+        public List<Phase> phaseTable = new List<Phase>();
+    }
+}
