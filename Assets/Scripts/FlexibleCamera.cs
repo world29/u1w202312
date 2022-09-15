b214f9f8095264ca60e14d54a56e66046e21ac8b
world29 <623p6236p@gmail.com>
2022-09-16 00:36:26 +0900

    アスペクト比を 16:9 に固定

---
+++ Assets/Scripts/FlexibleCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity1Week
+{
+    public class FlexibleCamera : MonoBehaviour
+    {
+        void Update()
+        {
+            FixAspect();
+        }
+
+        private void FixAspect()
+        {
+            Camera[] cameras = this.GetComponentsInChildren<Camera>();
+            float gameAspect = 16f / 9f;
+            float screenAspect = (float)Screen.width / (float)Screen.height;
+            float diff = gameAspect - screenAspect;
+
+            if (diff > Vector3.kEpsilon)
+            {
+                float height = (float)Screen.width / gameAspect;
+                float y = (Screen.height - height) * 0.5f;
+                Rect pixelRect = new Rect(0f, y, Screen.width, height);
+                for (int i = 0; i < cameras.Length; ++i)
+                {
+                    cameras[i].pixelRect = pixelRect;
+                }
+            }
+            else
+            {
+                float width = (float)Screen.height * gameAspect;
+                float x = (Screen.width - width) * 0.5f;
+                Rect pixelRect = new Rect(x, 0f, width, Screen.height);
+                for (int i = 0; i < cameras.Length; ++i)
+                {
+                    cameras[i].pixelRect = pixelRect;
+                }
+            }
+        }
+    }
+}
