4184f741599f9459c3e1726c47054e3fa25b820b
world29 <623p6236p@gmail.com>
2022-10-03 15:16:51 +0900

    u1w共有会のための動画撮影用機能

--- Assets/Scripts/Confine2DUpdater.cs
+++ Assets/Scripts/Confine2DUpdater.cs
@@ -9,8 +9,20 @@ namespace Unity1Week
         [SerializeField]
         private PlayerMovement playerMovement;
 
+        [SerializeField]
+        private bool demoEnabled;
+
+        [SerializeField]
+        private float scrollSpeed = 0.1f;
+
         void LateUpdate()
         {
+            if (demoEnabled)
+            {
+                transform.Translate(scrollSpeed * Time.deltaTime, 0, 0);
+                return;
+            }
+
             var newPos = transform.position;
             newPos.x = playerMovement.Position.x;
 
