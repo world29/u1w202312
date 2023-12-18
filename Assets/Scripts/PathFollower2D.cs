5031c8462d7df2a4fdceece0774d355af1b657eb
world29 <623p6236p@gmail.com>
2023-12-19 01:00:02 +0900

    線路上をうごく列車のモック

---
+++ Assets/Scripts/PathFollower2D.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+using PathCreation;
+
+namespace u1w202312
+{
+    // Moves along a path at constant speed.
+    // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
+    public class PathFollower2D : MonoBehaviour
+    {
+        public PathCreator pathCreator;
+        public EndOfPathInstruction endOfPathInstruction;
+        public float speed = 5;
+        public float offset = 0f;
+        float distanceTravelled;
+
+        void Start()
+        {
+            if (pathCreator != null)
+            {
+                // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
+                pathCreator.pathUpdated += OnPathChanged;
+
+                distanceTravelled = offset;
+            }
+        }
+
+        void Update()
+        {
+            if (pathCreator != null)
+            {
+                distanceTravelled += speed * Time.deltaTime;
+                transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+                var quat3D = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+                var euler3D = quat3D.eulerAngles;
+                transform.rotation = Quaternion.Euler(0, 0, -euler3D.x);
+            }
+        }
+
+        // If the path changes during the game, update the distance travelled so that the follower's position on the new path
+        // is as close as possible to its position on the old path
+        void OnPathChanged()
+        {
+            distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+        }
+    }
+}
