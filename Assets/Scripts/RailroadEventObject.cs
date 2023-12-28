f29c781d2cbb88248f9f47c01339059d57edd4c5
world29 <623p6236p@gmail.com>
2023-12-28 09:39:20 +0900

    線路上の人間が列車に跳ね飛ばされるようにした

---
+++ Assets/Scripts/RailroadEventObject.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+using PathCreation;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using DG.Tweening;
+
+namespace u1w202312
+{
+    public class RailroadEventObject : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector2 forceOnHit;
+
+        [SerializeField]
+        private float torqueOnHit;
+
+        private bool _isAlive = true;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _isAlive = false;
+
+                var rb = GetComponent<Rigidbody>();
+                rb.isKinematic = false;
+                rb.AddForce(forceOnHit, ForceMode.Impulse);
+                rb.AddTorque(new Vector3(0, 0, torqueOnHit), ForceMode.Impulse);
+
+                GetComponent<Collider>().enabled = false;
+
+                DOVirtual.DelayedCall(5f, () => Destroy(gameObject));
+            }
+        }
+    }
+}
